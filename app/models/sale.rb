8a4a5b0baaf5d43e643eba2e764736a9e9af1b90
ruby -v <malcon.moura.lima@gmail.com>
2017-03-23 02:07:37 -0300

    20170323_0207

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -3,4 +3,21 @@ class Sale < ApplicationRecord
   belongs_to :user
   belongs_to :discount
   has_many :product_quantities
+  has_one :comission
+
+  after_save do
+    calc = 0
+    # Soma o preço dos produtos vezes a quantidade deles
+    self.product_quantities.each {|p| calc += p.product.price * p.product.quantity}
+
+    # Verifica se existe um desconte e aplica caso exista
+    if self.discount
+      if self.discount.kind == 'porcent'
+        calc -= calc / self.discount.value
+      elsif self.discount.kind == 'money'
+        calc -= self.discount.value
+      end
+    end
+  end
+
 end
